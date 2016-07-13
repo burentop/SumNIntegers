5d812f5d85d793ae0f54c1202ebf1db36f9a66fb
Brent Perry <burentoperii@gmail.com>
2016-07-13 10:29:16 -0500

    Initial commit of SumNIntegers.

--- SumNIntegers/main.cpp
+++ SumNIntegers/main.cpp
@@ -6,10 +6,26 @@
 //  Copyright © 2016 Brent Perry. All rights reserved.
 //
 
-#include <iostream>
+#include "../../../std_lib_facilities.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    vector<int> to_sum;
+    int to_load, n;
+    int summed = 0;
+    
+    cout << "Please enter the number of values you want to sum. \n";
+    cin >> n;
+    
+    cout << "Please enter some integers (press '|' to stop): \n";
+    while (cin >> to_load) {
+        to_sum.push_back(to_load);
+    }
+    
+    cout << "The sum of the first " << n << " numbers ( ";
+    for (int i = 0; i < n; ++i) {
+        cout << to_sum[i] << " ";
+        summed += to_sum[i];
+    }
+    cout << ") is " << summed << ".\n";
 }
